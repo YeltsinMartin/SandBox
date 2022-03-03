adb6a8018b04c163d3d247a4f4394b2a067e1507
Yeltsin Martin Babu <yelasterix@gmail.com>
2022-03-03 22:38:25 +0530

    vector string practice

--- C++/basic/main.cpp
+++ C++/basic/main.cpp
@@ -1,18 +1,28 @@
 #include<iostream>
+#include<string>
+#include<vector>
 
 using namespace std;
 
 unsigned long fibonacci(int n){
-    if(n==0) return 0;
-    if(n<=2) return 1;
+    
+    if(n<=2) 
+    {
+        if(n<=0) return 0;
+        return 1;
+    }
     return fibonacci(n-1) + fibonacci(n-2);
 }
 
 int main()
 {
     cout <<"Parise the Lord\n";
+
     cout << "Fibonacci 10: "<<fibonacci(10)<<endl;
-    cout << "Fibonacci 6: "<<fibonacci(6)<<endl;
-    cout << "Fibonacci 5: "<<fibonacci(5)<<endl;
+
+    vector<string> name = {"Yeltsin","Martin", "Babu", "Welcomes", "You"} ;
+
+    for (auto &i:name)  cout << i <<" ";
+
     return 0;
 }
